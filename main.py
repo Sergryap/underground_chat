8d08e9d753574b6fbbda22556db8a06bb3265704
Sergryap <rs1180@mail.ru>
2023-03-23 10:51:58 +0500

    Save messages history to file

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 import asyncio
+import aiofiles
+from datetime import datetime
 
 
-async def tcp_echo_client(message):
+async def tcp_echo_client(message, file):
     reader, writer = await asyncio.open_connection('minechat.dvmn.org', 5000)
     while True:
         try:
-            print(f'Send: {message!r}')
-            writer.write(message.encode())
-            await writer.drain()
+            # print(f'Send: {message!r}')
+            # writer.write(message.encode())
+            # await writer.drain()
             data = await reader.read(100)
-            print(data.strip().decode("utf-8-sig", "ignore"))
+            now_time = datetime.now().strftime("[%d.%m.%Y %H:%M:%S]")
+            msg = data.strip().decode("utf-8-sig", "ignore")
+            async with aiofiles.open(file, mode='a') as f:
+                await f.write(f'{now_time} {msg}\n')
+            print(f'{now_time} {msg}')
         except KeyboardInterrupt:
             writer.close()
             await writer.wait_closed()
@@ -17,4 +23,4 @@ async def tcp_echo_client(message):
 
 
 if __name__ == '__main__':
-    asyncio.run(tcp_echo_client('Hello World!'))
+    asyncio.run(tcp_echo_client('Hello World!', 'chat.txt'))
